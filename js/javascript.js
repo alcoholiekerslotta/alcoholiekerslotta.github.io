2bd4c96ac4dcc63e4c33f778f88f8f6601de2523
alcoholiekerslotta <50381231+alcoholiekerslotta@users.noreply.github.com>
2019-05-07 21:56:30 +0200

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,14 @@
+function audio(){
+    var audio = new Audio('../sounds/watbenjijlelijk.mp3');
+    audio.play();
+}
+
+function audio1(){
+    var audio = new Audio('../sounds/lastigepot.mp3');
+    audio.play();
+}
+
+function audio2(){
+    var audio = new Audio('../sounds/porno, porno.mp3');
+    audio.play();
+}
